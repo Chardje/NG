7c9a9866f02599d85a492e9dfadbaf95bd3c1859
Dmytro Sheyko <dmytro.sheyko@localhost>
2021-07-23 19:58:53 +0300

    Операції з вектором, колір у об'єкта

--- NG_V0.0.0/ObjectClass.cs
+++ NG_V0.0.0/ObjectClass.cs
@@ -11,14 +11,20 @@ namespace NG_V0._0._0
 {
     internal abstract class Object
     {
-        internal Vector center;
+        internal readonly Vector center;
         internal Vector vel;
         internal Vector spin;
         internal long mass;
         internal List<Object> constituents;
-        internal Object(Vector center)
+        internal readonly Color color;
+        internal Object(Vector center)  : this(center, new Color { R = 255, G = 255, B = 255 })
+        {
+            // nothing
+        }
+        internal Object(Vector center, Color color)
         {
             this.center = center;
+            this.color = color;
         }
 
         internal abstract bool ObjectInter(Ray ray, out double t0, out double t1);
@@ -26,22 +32,26 @@ namespace NG_V0._0._0
     internal class Sphere : Object
     {
         int Radius;
-        internal Sphere(Vector center,int Radius):base(center)
+        internal Sphere(Vector center, int Radius) : base(center)
+        {
+            this.Radius = Radius;
+        }
+        internal Sphere(Vector center, Color color, int Radius) : base(center, color)
         {
             this.Radius = Radius;
         }
 
         internal override bool ObjectInter(Ray ray,out double t0, out double t1)
         {
-            double x0 = center.x;
-            double y0 = center.y;
-            double z0 = center.z;
-            double x1 = ray.Origin.x;
-            double y1 = ray.Origin.y;
-            double z1 = ray.Origin.z;
-            double x2_x1 = ray.Direction.x;
-            double y2_y1 = ray.Direction.y;
-            double z2_z1 = ray.Direction.z;
+            double x0 = center.X;
+            double y0 = center.Y;
+            double z0 = center.Z;
+            double x1 = ray.Origin.X;
+            double y1 = ray.Origin.Y;
+            double z1 = ray.Origin.Z;
+            double x2_x1 = ray.Direction.X;
+            double y2_y1 = ray.Direction.Y;
+            double z2_z1 = ray.Direction.Z;
 
             return Math.SquareEquation.Solve(
                 M.Pow(x2_x1, 2) + M.Pow(y2_y1, 2) + M.Pow(z2_z1, 2),
@@ -52,7 +62,7 @@ namespace NG_V0._0._0
 
         bool CollidesWith(Sphere b)
         {
-            return M.Sqrt((center - b.center).x) + M.Sqrt((center - b.center).y) + M.Sqrt((center - b.center).z) <= M.Sqrt(Radius + b.Radius);
+            return M.Sqrt((center - b.center).X) + M.Sqrt((center - b.center).Y) + M.Sqrt((center - b.center).Z) <= M.Sqrt(Radius + b.Radius);
         }
     }
 }
---
+++ NG_V0.0.0Tests/VectorTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NG_V0._0._0;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NG_V0._0._0.Tests
+{
+    [TestClass()]
+    public class VectorTests
+    {
+        [DataTestMethod]
+        [DataRow(0, 0, 0, 0)]
+        [DataRow(1, 0, 0, 1)]
+        [DataRow(0, 1, 0, 1)]
+        [DataRow(0, 0, 1, 1)]
+        [DataRow(-100, 0, 0, 100)]
+        [DataRow(0, -200, 0, 200)]
+        [DataRow(0, 0, -300, 300)]
+        [DataRow(3, -4, 0, 5)]
+        [DataRow(0, -15, 20, 25)]
+        [DataRow(-8, 0, 6, 10)]
+        [DataRow(1, -1, 0, 1.414213)] // sqrt(2) = 1.414213
+        public void LengthTest(double x, double y, double z, double length)
+        {
+            double actual = new Vector(x, y, z).Length;
+            Assert.AreEqual(length, actual, 1e-6);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 0, 0, 1, 0, 0)]
+        [DataRow(0, 1, 0, 0, 1, 0)]
+        [DataRow(0, 0, 1, 0, 0, 1)]
+        [DataRow(-1, 0, 0, -1, 0, 0)]
+        [DataRow(0, -1, 0, 0, -1, 0)]
+        [DataRow(0, 0, -1, 0, 0, -1)]
+        [DataRow(10, 0, 0, 1, 0, 0)]
+        [DataRow(0, 20, 0, 0, 1, 0)]
+        [DataRow(0, 0, 30, 0, 0, 1)]
+        [DataRow(-40, 0, 0, -1, 0, 0)]
+        [DataRow(0, -50, 0, 0, -1, 0)]
+        [DataRow(0, 0, -60, 0, 0, -1)]
+        [DataRow(1, 1, 1, 0.57735, 0.57735, 0.57735)] // 1/sqrt(3) = 0.57735
+        public void NormTest(double x, double y, double z, double xn, double yn, double zn)
+        {
+            Vector norm = new Vector(x, y, z).Norm;
+            Assert.IsTrue(AreAlmostEqual(xn, norm.X, 1e-6) && AreAlmostEqual(yn, norm.Y, 1e-6) && AreAlmostEqual(zn, norm.Z, 1e-6), $"Expected ({xn}, {yn}, {zn}), Actual ({norm.X}, {norm.Y}, {norm.Z})");
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 0, 0, 1, 0, 0, 1)]
+        [DataRow(0, 3, 0, 0, 2, 0, 6)]
+        [DataRow(0, 0, -3, 0, 0, -4, 12)]
+        [DataRow(0, 0, 3, 0, 0, -4, -12)]
+        [DataRow(1, 0, 0, 0, 1, 0, 0)]
+        public void ScalarProductTest(double x0, double y0, double z0, double x1, double y1, double z1, double product)
+        {
+            double actual = new Vector(x0, y0, z0) * new Vector(x1, y1, z1);
+            Assert.AreEqual(product, actual, 1e-6);
+        }
+
+
+        private static bool AreAlmostEqual(double a, double b, double e)
+        {
+            return System.Math.Abs(a - b) < e;
+        }
+    }
+}
